ab07f822ce56606ed9171a0cc73e208bcf709bc2
Leandro Baseggio <leandrobaseggio@gmail.com>
2021-03-16 11:13:26 +0000

    Added Sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
@@ -11,8 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to
-          <a href="https://nextjs.org">Next.js!</a>
+          My Posts
         </h1>
 
         <p className={styles.description}>
