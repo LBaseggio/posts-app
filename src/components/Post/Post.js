433c4508b9d16640b5e0f7b22e6fb5188a68725e
Leandro Baseggio <leandrobaseggio@gmail.com>
2021-03-18 20:43:30 +0000

    Connected Likes to Airtable. No function yet

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,7 +1,7 @@
 import { FaHeart, FaShareAlt } from 'react-icons/fa';
 import styles from './Post.module.scss';
 
-const Post = ({ content, date }) => (
+const Post = ({ content, date, like }) => (
   <>
     <p className={styles.postContent}>
       {content}
@@ -9,7 +9,7 @@ const Post = ({ content, date }) => (
     <ul className={styles.postMeta}>
       <li className={styles.postMetaData}>
         <FaHeart />
-        23
+        {like}
       </li>
       <li className={styles.postMetaData}>
         <FaShareAlt />
